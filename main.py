fd84bc45eed8556be1942271a8586bfd5ce3a917
Trijai <164654228+Trijai@users.noreply.github.com>
2024-04-13 09:34:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,32 +9,40 @@ from sklearn.model_selection import train_test_split
 from cvzone.FaceDetectionModule import FaceDetector
 
 # Create a path variable to the path of your dataset
-
+path = "Pneumothorax-New-Dataset
 
 # Create empty images list and categories list
+infected = []
 
+uninfected = []
 
 # Loop throught each img in path
-
+for img in os.listdir(path):
     # Add try block
-    
+    try:
         # Print the img
-        
+        print(img)
     
         # Split the img address from "_" and get the first character and store in type variable
-        
+        type = img.split("_")[0]
         # Load the image
-        
+        img = cv2.imread(str(path)+"/"+str(img))
         # Change image from BGR to RGB
-        
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
         # Resize the image to 200,200
         
         # Append the image to images list
-        
+        if type == '0':
+            infected.append(img)
+    else :
         # Append the type to categories list
         
     # Add except block  
-    
+    except:
+        print("error in reading")
 
 # Print count of all image i.e len(images)
+print("Count of uninfected images", len(uninfected))
+print("Count of uninfected images", len(uninfected))
+
 
